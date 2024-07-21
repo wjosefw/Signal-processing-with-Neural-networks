c750929cc599c46855f7a6822d7e4109ce9df5a0
josea <javellan@ucm.es>
2024-07-21 21:50:36 +0200

    Global commit

--- Models.py
+++ Models.py
@@ -3,27 +3,27 @@ import numpy as np
 import tensorflow as tf
 tf.version.VERSION
 
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input, Layer, Lambda
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, concatenate
 from tensorflow.keras import initializers
 from keras.regularizers import l2
-
+from tensorflow.keras.initializers import Constant
 
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
-def model_definition_manual_weight_initialization(N_time_points, NM, weights):
+def model_definition_manual_weight_initialization(N_time_points, kernel_weights, NM = 12, NN = 32, STD_INIT = 1):
 
   inputA = Input(shape=(N_time_points,))
   inputB = Input(shape=(N_time_points,))
 
-  x1 = Dense(NM, activation="relu", weights=[weights, np.zeros(NM)])
-  #x1 = Dense(NM, activation="relu", kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=1.5), kernel_regularizer=l2(0.01))
-  x2 = Dense(4, activation="relu", kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=1.5), kernel_regularizer=l2(0.01))
-  x3 = Dense(4, activation="relu", kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=1.5), kernel_regularizer=l2(0.01))
-  x4 = Dense(4, activation="relu", kernel_initializer=initializers.RandomNormal(mean=0.0, stddev=1.5), kernel_regularizer=l2(0.01))
-  x5 = Dense(1, activation="linear")
+  x1 = Dense(NM, activation="relu", kernel_initializer= Constant(kernel_weights), bias_initializer='zeros')
+  #x1 = Dense(NM, activation = "relu", kernel_initializer=initializers.RandomNormal(mean = 0.0, stddev = STD_INIT), kernel_regularizer = l2(0.01))
+  x2 = Dense(NN, activation = "relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT), kernel_regularizer = l2(0.01))
+  x3 = Dense(NN, activation = "relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT), kernel_regularizer = l2(0.01))
+  x4 = Dense(NN, activation = "relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT), kernel_regularizer = l2(0.01))
+  x5 = Dense(1, activation = "linear")
 
   op1A = x1(inputA)
   op2A = x2(op1A)
@@ -37,14 +37,14 @@ def model_definition_manual_weight_initialization(N_time_points, NM, weights):
   op4B = x4(op3B)
   outB = x5(op4B)
 
-  model = Model(inputs=[inputA, inputB], outputs=outA-outB)
-  return model
+  model = Model(inputs = [inputA, inputB], outputs = outA-outB)
+  return model 
 
 
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
-def model_definition(NM, NN = 128, STD_INIT = 1):
+def model_definition(NM = 12, NN = 128, STD_INIT = 1):
 
   inputA = Input(shape=(NM,))
   inputB = Input(shape=(NM,))
@@ -64,5 +64,90 @@ def model_definition(NM, NN = 128, STD_INIT = 1):
   op3B = x3(op2B)
   outB = x4(op3B)
 
-  model = Model(inputs=[inputA, inputB], outputs=outA-outB)
+  model = Model(inputs = [inputA, inputB], outputs = outA - outB)
+  return model
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+
+def model_definition_HEinitialization(NM = 12, NN = 128):
+
+  inputA = Input(shape=(NM,))
+  inputB = Input(shape=(NM,))
+
+  x1 = Dense(NN, activation="relu", kernel_initializer = initializers.HeNormal(), kernel_regularizer = l2(0.01))
+  x2 = Dense(NN, activation="relu", kernel_initializer = initializers.HeNormal(), kernel_regularizer = l2(0.01))
+  x3 = Dense(NN, activation="relu", kernel_initializer = initializers.HeNormal(), kernel_regularizer = l2(0.01))
+  x4 = Dense(1, activation="linear")
+
+  op1A = x1(inputA)
+  op2A = x2(op1A)
+  op3A = x3(op2A)
+  outA = x4(op3A)
+
+  op1B = x1(inputB)
+  op2B = x2(op1B)
+  op3B = x3(op2B)
+  outB = x4(op3B)
+
+  model = Model(inputs=[inputA, inputB], outputs = outA-outB)
+  return model
+
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+
+def model_definition_convolutional(N_points):
+
+  input_shape = (2,N_points,1)
+
+
+  model = Sequential()
+  model.add(Conv2D(filters = 16, kernel_size=(2, 2), activation='relu', padding = 'same', input_shape=input_shape)) #ouput_dim = (2, N_points, 4)
+  model.add(MaxPooling2D((2, 2))) # Halves the dimensions
+  model.add(Conv2D(filters = 32, kernel_size=(2, 2), activation='relu', padding = 'same')) #ouput_dim = (1, N_points/2, 4)
+  model.add(MaxPooling2D((1, 2)))
+  model.add(Flatten())
+  model.add(Dense(4, activation='relu'))
+  model.add(Dense(1, activation='linear'))
+  return model
+
+
+#----------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------
+
+def model_definition_four_inputs(NM = 12, NN = 32, STD_INIT = 1):
+
+  inputA = Input(shape=(NM,))
+  inputB = Input(shape=(NM,))
+  inputC = Input(shape=(NM,))
+  inputD = Input(shape=(NM,))
+
+  x1 = Dense(NN, activation="relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT) , kernel_regularizer = l2(0.01))
+  x2 = Dense(NN, activation="relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT) , kernel_regularizer = l2(0.01))
+  x3 = Dense(NN, activation="relu", kernel_initializer = initializers.RandomNormal(mean = 0.0, stddev = STD_INIT) , kernel_regularizer = l2(0.01))
+  x4 = Dense(1, activation="relu")
+
+  op1A = x1(inputA)
+  op2A = x2(op1A)
+  op3A = x3(op2A)
+  outA = x4(op3A)
+
+  op1B = x1(inputB)
+  op2B = x2(op1B)
+  op3B = x3(op2B)
+  outB = x4(op3B)
+
+  op1C = x1(inputC)
+  op2C = x2(op1C)
+  op3C = x3(op2C)
+  outC = x4(op3C)
+
+  op1D = x1(inputD)
+  op2D = x2(op1D)
+  op3D = x3(op2D)
+  outD = x4(op3D)
+
+  final_output = concatenate([outA, outB, outC, outD], axis=-1)
+  model = Model(inputs=[inputA, inputB, inputC, inputD], outputs=final_output)
   return model
--- functions_KAN.py
+++ functions_KAN.py
@@ -5,16 +5,19 @@ import numpy as np
 #----------------------------------------------------------------------------------------------
 
 
-def train_loop_KAN(model, optimizer, train_loader, test_tensor, EPOCHS = 75):
+def train_loop_KAN(model, optimizer,  train_loader, test_tensor, limit = 5, EPOCHS=75, checkpoint=15, name='model', save=False):
     """
     Train a KAN Neural Network model for a specified number of epochs.
 
     Args:
-    model: The KAN model to be trained.
+    model (torch.nn.Module): The KAN model to be trained.
     optimizer (torch.optim.Optimizer): The optimizer used to adjust the model's parameters.
     train_loader (torch.utils.data.DataLoader): DataLoader containing the training data.
     test_tensor (torch.Tensor): Tensor used for testing/validation.
     EPOCHS (int, optional): The number of epochs to train the model. Default is 75.
+    checkpoint (int, optional): The frequency (in epochs) at which the model is saved. Default is 15.
+    name (str, optional): Base name for the saved model files. Default is 'model'.
+    save (bool, optional): Whether to save the model at specified checkpoints. Default is False.
 
     Returns:
     tuple: A tuple containing:
@@ -24,84 +27,80 @@ def train_loop_KAN(model, optimizer, train_loader, test_tensor, EPOCHS = 75):
     The function trains the model by iterating over the training data for a specified number of epochs.
     It calculates the loss, performs backpropagation, and updates the model parameters. After each epoch,
     it evaluates the model on the test_tensor and stores the predictions and loss values.
+    If the 'save' parameter is set to True, the model is saved at the specified checkpoint intervals.
     """
 
-    epoch_number = 0
     loss_list = []
     test = []
 
     for epoch in range(EPOCHS):
+        running_loss = 0.0
+        avg_loss_epoch = 0.0
 
-        running_loss = 0.
-        avg_loss_epoch = 0.
-    
         for i, data in enumerate(train_loader):
-            
             # Every data instance is an input + label pair
             inputs, labels = data
-        
-            # Zero your gradients for every batch!
+
+            # Zero your gradients for every batch
             optimizer.zero_grad()
-        
-            # Make predictions for this batch
-            outputs_0 = model(inputs[:,:,0])
-            outputs_1 = model(inputs[:,:,1])
+
+            # Make predictions for this batch for both channels
+            outputs_0 = model(inputs[:, :, 0])
+            outputs_1 = model(inputs[:, :, 1])
 
             # Compute the loss and its gradients
-            loss = torch.mean((outputs_0 - outputs_1 - labels)**2) + torch.sum(torch.relu(-outputs_0)) + torch.sum(torch.relu(-outputs_1))
+            loss = torch.mean((outputs_0 - outputs_1 - labels) ** 2) + torch.sum(torch.relu(-outputs_0)) + torch.sum(torch.relu(-outputs_1))
+
             loss.backward()
-        
+
             # Adjust learning weights
             optimizer.step()
-        
-            # Gather data and report
+
+            # Accumulate running loss
             running_loss += loss.item()
 
-    
-        avg_loss_epoch = running_loss / int(i) # loss per batch        
+        # Calculate average loss per epoch
+        avg_loss_epoch = running_loss / int(i)  # loss per batch
         loss_list.append(avg_loss_epoch)
-        
-        print('EPOCH {}:'.format(epoch_number + 1))
+
+        print('EPOCH {}:'.format(epoch + 1))
         print('LOSS train {}'.format(avg_loss_epoch))
-        
-        # Calculate predictions
-        test_epoch = model(test_tensor)
-        test.append(np.squeeze(test_epoch.detach().numpy()))
 
-        epoch_number += 1
+        # Calculate predictions on test_tensor
+        with torch.no_grad():
+            test_epoch = model(test_tensor)
+            test.append(np.squeeze(test_epoch.detach().numpy()))
 
-    # Turn array to list
-    loss_array = np.array(loss_list, dtype = 'object')
-    test = np.array(test, dtype = 'object')
+        # Save the model at the specified checkpoint frequency if 'save' is True
+        if save:
+            if epoch % checkpoint == 0:
+                model_name = name + '_' + str(epoch)
+                torch.save(model.state_dict(), model_name)
+
+    # Convert lists to numpy arrays
+    loss_array = np.array(loss_list, dtype='object')
+    test = np.array(test, dtype='object')
 
     return loss_array, test
 
-
 #----------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
-def n_params_KAN(architecture, grid_size):
-    multiplication_array = np.zeros((len(architecture)-1))
-    for i in range(len(architecture) - 1):
-        multiplication_array[i] = architecture[i]*architecture[i+1]
-    
-    n_params = grid_size*np.sum(multiplication_array)
-    return int(n_params)
 
-
-def n_params_KAN(architecture, grid_size):
+def n_params_KAN(architecture, grid_size, order):
     """
     Calculate the number of parameters for a given KAN neural network architecture.
 
     Args:
     architecture (list of int): A list where each element represents the number of neurons in each layer of the neural network.
     grid_size (int): A scalar value that represents the grid size used in the KAN model.
+    order (int): A scalar value representing the order of the polynomial splines.
 
     Returns:
     int: The total number of parameters in the KAN model.
 
     The function calculates the number of parameters by multiplying the number of neurons between consecutive layers
-    and summing these products, then multiplying the sum by the grid size.
+    and summing these products, then multiplying the sum by the grid size plus order.
     """
     # Initialize an array to hold the products of neurons between consecutive layers
     multiplication_array = np.zeros((len(architecture) - 1))
@@ -110,7 +109,7 @@ def n_params_KAN(architecture, grid_size):
     for i in range(len(architecture) - 1):
         multiplication_array[i] = architecture[i] * architecture[i + 1]
     
-    # Sum the products and multiply by the grid size to get the total number of parameters
-    n_params = grid_size * np.sum(multiplication_array)
+    # Sum the products and multiply by the grid size plus order to get the total number of parameters
+    n_params = (grid_size + order) * np.sum(multiplication_array)
     
     return int(n_params)
